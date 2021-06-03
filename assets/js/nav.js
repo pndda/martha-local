099f619a5a22d7d740e998aa04cd896ea2594038
Panadda Manarata <Panadda@Panaddas-MacBook-Pro.local>
2021-06-03 17:12:58 +0200

    x

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,33 @@
 $(window).scroll(function () {
-
-  if ($(this).scrollTop() > 50) {
-      $('#mainNav').addClass("content_fixed");
+  if ($(window).width() < 767) {
+    console.log('hello');
+    if ($(this).scrollTop() > 100) {
+      $('#mainNav').addClass("content_fixed-mob");
+    } else {
+      $('#mainNav').removeClass("content_fixed-mob");
+    }
   } else {
+
+  //   $(function(){
+  //     $(document).scroll(function(){
+  //         if($(this).scrollTop() >= $('#mainNav').offset().top - 450) {
+  //             $("#mainNav").css("background","red");
+  //         } else {
+  //             $("#mainNav").css("background","orange");
+  //         }
+  //     });
+  // });
+
+    if ($(this).scrollTop() > 750) {
+      $('#mainNav').addClass("content_fixed");
+    } else {
       $('#mainNav').removeClass("content_fixed");
+    }
   }
+
+  // if ($(this).scrollTop() > 50) {
+  //     $('#mainNav').addClass("content_fixed");
+  // } else {
+  //     $('#mainNav').removeClass("content_fixed");
+  // }
 });
